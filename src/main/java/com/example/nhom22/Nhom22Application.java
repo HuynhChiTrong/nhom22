00d4fd71b4c37daa76d2f45cf8d472e0b09479d6
huynhchitrong2180603154 <huynhchitrong03@gmail.com>
2024-06-03 14:38:37 +0700

    Initial commit

---
+++ src/main/java/com/example/nhom22/Nhom22Application.java
@@ -0,0 +1,13 @@
+package com.example.nhom22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom22Application.class, args);
+	}
+
+}
